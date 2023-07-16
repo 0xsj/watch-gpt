78143eb0eab1778f175f296bc2420d709bc142de
sjtommylee <sjtommylee@gmail.com>
2023-07-15 23:56:14 -0700

    chore: commit save point

--- apps/feature-news/src/main.ts
+++ apps/feature-news/src/main.ts
@@ -1,22 +1,30 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 import { AppModule } from './app/app.module';
 
+const logger = new Logger();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'news-service',
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'news-service',
+        },
+      },
+    }
   );
+
+  logger.log('News-service is listening!');
+  await app.listen();
 }
 
 bootstrap();
