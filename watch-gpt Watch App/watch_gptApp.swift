a41352e280185b6c7c9ae3804f0deeb702e96d08
sjtommylee <sjtommylee@gmail.com>
2023-09-17 13:12:18 -0700

    Initial Commit

---
+++ watch-gpt Watch App/watch_gptApp.swift
@@ -0,0 +1,17 @@
+//
+//  watch_gptApp.swift
+//  watch-gpt Watch App
+//
+//  Created by Tommy Lee on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct watch_gpt_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
