c2241e5c96cea0e49333cfe4a3ce62a77d1dcfa1
sjtommylee <sjtommylee@gmail.com>
2023-09-24 13:04:24 -0700

    chore: commit save point

--- shared/ResponseParsingTask.swift
+++ shared/ResponseParsingTask.swift
@@ -12,6 +12,9 @@ actor ResponseParsingTask {
 
   func parse(text: String) async -> AttributedOutput {
     let document = Document(parsing:text)
+    var markdownParser = MarkdownAttributedStringParser()
+    let results = markdownParser.parserResults(from:document)
+    return AttributedOutput(string: text, results: result)
   }
 
 }
