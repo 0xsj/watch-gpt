bdb1a7266449cc79a2c63398861cf302b692782e
sjtommylee <sjtommylee@gmail.com>
2023-10-04 14:20:05 -0700

    chore: commit save point

--- shared/Loader.swift
+++ shared/Loader.swift
@@ -13,4 +13,19 @@ struct Loader : View {
     @State private var showDot1 = false
     @State private var showDot2 = false 
     @State private var showDot3 = false
+
+    var body: some View {
+        HStack {
+            Circle()
+                .opacity(showDot1 ? 1 : 0)
+            Circle()
+                .opacity(showDot2 ? 1 : 0)
+            Circle()
+                .opacity(showDot3 ? 1 : 0)
+        }
+        .foregroundColor(.gray.opacity(0.5))
+        .onAppear {performAnimation()}
+    }
+
+    func performAnimation() {}
 }
